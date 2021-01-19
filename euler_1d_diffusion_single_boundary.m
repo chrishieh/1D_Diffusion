73d17df443f33fb6d2102afedcea8b8f59798c6a
Christopher Shieh <slapshot811@gmail.com>
2021-01-19 09:19:58 -0500

    single boundary

--- euler_1d_diffusion_single_boundary.m
+++ euler_1d_diffusion_single_boundary.m
@@ -1,17 +1,18 @@
 L = .1; %length in meters
 n = 20; %number of simulation nodes
 T0 = 20+273; %initial temperature throughout bar in celcius
+t_final = 100; %simulation time in seconds
 
 material = 1; %0=steel, 1=aluminum
 
 animation_plot = false;
-over_xl_plot = true;
-over_x0_plot = false;
+over_xl_plot = false;
+over_x0_plot = true;
 over_time_plot = false;
 
 
-T1 = 20+273; %Surface temp 1
-T2 = 100+273; %Surface temp 2
+T1 = 393; %Surface temp 1
+%T2 = 100+273; %Surface temp 2
 
 dx = L/n; %node side in meters
 
@@ -28,43 +29,37 @@ if material == 0
 elseif material == 1
     alpha = al_thermal_conductivity / (al_density * al_specific_heat);
 end
-disp(alpha);
 
-t_final = 2000; %simulation time in seconds
 
 dt = .1;% time step in seconds
 
-x = dx/2:dx:L+dx/2;
+x = dx/2:dx:L;
 
 T = ones(n,1) * T0;
-size(x)
-size(T)
+%size(x)
+%size(T)
 dTdt = zeros(n, 1);
 
 t = 0:dt:t_final;
-start_temp = [T0];
-end_temp = [T1];
+first_node_temp = [T0];
+end_temp = [T0];
 time = [0];
 for j = 1:length(t)
-    for i = 1:n-1
-        if i == 1
-            dTdt(i) = alpha * ((T(i+1)-T(i)) / dx^2);
-        else
-            dTdt(i) = alpha *(-(T(i) - T(i-1)) / dx^2 + (T(i+1)-T(i)) / dx^2);
-        end
+    for i = 2:n-1
+        dTdt(i) = alpha *(-(T(i) - T(i-1)) / dx^2 + (T(i+1)-T(i)) / dx^2);
     end
-    %dTdt(1) = dTdt(2);
-    dTdt(n) = alpha *(-(T(n) - T(n-1)) / dx^2 + (T2-T(n)) / dx^2);
+    dTdt(1) = alpha *(-(T(1) - T1) / dx^2 + (T(2)-T(1)) / dx^2);
+    dTdt(n) = alpha *(-(T(n) - T(n-1)) / dx^2);
     T = T+dTdt*dt;
     newT = T;
-    newT(n+1) = T2;
-    start_temp(end+1) = T(1);
+    newT(1) = T1;
+    first_node_temp(end+1) = T(1);
     end_temp(end+1) = T(end);
     time(end+1) = j*dt;
     if animation_plot
         figure(1);
-        plot(x, newT, 'Linewidth', 3);
-        axis([0 L*1.05 -10 100]);
+        plot(x, T, 'Linewidth', 3);
+        axis([0 L*1.05 min(T1, T0) max(T1, T0)]);
         xlabel(dt* j);
         ylabel('Temperature (\circC)');
     end
@@ -77,23 +72,23 @@ if over_time_plot
     xlabel('x location (meters)');
     ylabel('Temperature (\circC)');
 end
-if over_x0_plot
+if over_xl_plot
     figure(3);
     plot(time, end_temp, 'Linewidth', 3);
     axis([0, t_final, T0, max(end_temp)]);
-    title('Temperature at x=0 Over Time')
-    xlabel('Time (s)');
-    ylabel('Temperature (\circC)');
-end
-if over_xl_plot
-    figure(4);
-    plot(time, start_temp, 'Linewidth', 3);
-    axis([0, t_final, T0, max(start_temp)]);
     title('Temperature at x=L Over Time')
     xlabel('Time (s)');
     ylabel('Temperature (\circC)');
 end
+if over_x0_plot
+    figure(4);
+    plot(time, first_node_temp, 'Linewidth', 3);
+    axis([0, t_final, T0, max(first_node_temp)]);
+    title('Temperature at x=0 Over Time')
+    xlabel('Time (s)');
+    ylabel('Temperature (\circC)');
+end
 T = table(start_temp);
-writetable(T,'C:\Users\Christopher\Desktop\REHTi\1D Diffusion\myDataxl.csv');
+writetable(T,'myDataxl.csv');
 disp(max(start_temp))
 disp(min(start_temp))
